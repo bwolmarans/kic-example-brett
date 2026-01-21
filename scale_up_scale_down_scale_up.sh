01463507de802257814aa8dc75945e0aeed01aab
bwolmarans <itsbrett@gmail.com>
2026-01-21 03:03:12 +0000

    see diff

---
+++ scale_up_scale_down_scale_up.sh
@@ -0,0 +1,36 @@
+!#/bin/bash
+echo "********************************************"
+echo "Scaling Up to Three (3) Instances of the Application"
+echo "********************************************"
+kubectl apply -f ./3-separate-nginx-pods-and-services.yaml
+sleep 2
+kubectl get svc -A
+sleep 3
+echo "********************************************"
+echo "Running our \"controller\" to query the K8s API, find the services, and update the external Kong Gw to match the scaling."
+echo "********************************************"
+sleep 3
+python3 k8s_api_python_example.py
+sleep 3
+echo "********************************************"
+echo "Running traffic through the external Kong Gw (acting as the LoadBalancer)"
+sleep 3
+for i in {1..10}; do curl 127.0.0.1:8000/1; done
+echo "********************************************"
+echo "Scaling down to One (1) instance of the Application"
+echo "********************************************"
+kubectl delete svc service-pod-2
+kubectl delete svc service-pod-3
+sleep 3
+echo "********************************************"
+echo "Running our \"controller\" to query the K8s API, find the services, and update the external Kong Gw to match the scaling."
+echo "********************************************"
+sleep 3
+python3 k8s_api_python_example.py
+sleep 3
+echo "********************************************"
+echo "Running traffic through the external Kong Gw (acting as the LoadBalancer)"
+sleep 3
+for i in {1..10}; do curl 127.0.0.1:8000/1; done
+echo "********************************************"
+
